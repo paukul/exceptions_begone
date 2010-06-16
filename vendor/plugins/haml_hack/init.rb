3f526f55ff0cbf5336e84ba5278a6b2bf5e799f3
Pascal Friederich <paukul@gmail.com>
2010-06-17 00:39:26 +0200

    I have no clue what that was for...

--- vendor/plugins/haml_hack/init.rb
+++ vendor/plugins/haml_hack/init.rb
@@ -1,12 +1,12 @@
-require 'haml/engine'
-
-module Haml::Precompiler
-  alias original_flush_merged_text flush_merged_text
-
-  def flush_merged_text
-    @to_merge.each do |item|
-      item[1].chomp! if (item[1].is_a?(String) && item[1] != "<!DOCTYPE html>\n")
-    end
-    original_flush_merged_text
-  end
-end
+# require 'haml/engine'
+# 
+# module Haml::Precompiler
+#   alias original_flush_merged_text flush_merged_text
+# 
+#   def flush_merged_text
+#     @to_merge.each do |item|
+#       item[1].chomp! if (item[1].is_a?(String) && item[1] != "<!DOCTYPE html>\n")
+#     end
+#     original_flush_merged_text
+#   end
+# end
